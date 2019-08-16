b9d1bf93a7a1efa7097e0606d310a9b2faf04704
shenglin-feng <slfeng123@icloud.com>
2019-08-16 15:05:45 +0800

    git init

---
+++ src/main/java/com/qdu/youda/YoudaApplication.java
@@ -0,0 +1,13 @@
+package com.qdu.youda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoudaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoudaApplication.class, args);
+    }
+
+}
